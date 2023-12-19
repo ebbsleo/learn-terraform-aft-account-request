1a2c15233c5151a8055b70f47e0ea62847b3d102
ebbsleo <35679628+ebbsleo@users.noreply.github.com>
2023-12-19 15:26:17 -0800

    Append new Terraform module for sandbox account

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,30 @@
 
+
+
+module "dc-1219" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "ebbeytt+dc1219@amazon.com"
+    AccountName               = "dc-1219"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "ebbeytt+dc1219@amazon.com"
+    SSOUserFirstName          = "Ebbey"
+    SSOUserLastName           = "Thomas"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "security"
+}
