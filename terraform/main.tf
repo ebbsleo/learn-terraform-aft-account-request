cd26665f66882090ea65c59473d93e67a680ecba
ebbsleo <35679628+ebbsleo@users.noreply.github.com>
2024-01-03 12:38:50 -0800

    Append new Terraform module for ebbeytt+b234

--- terraform/main.tf
+++ terraform/main.tf
@@ -223,3 +223,28 @@ module "ebbeytt+342" {
 
   account_customizations_name = "Infrastructure-ebbeytt+342"
 }
+
+
+module "ebbeytt+b234" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "ebbeytt+b234@amazon.com"
+    AccountName               = "ebbeytt+b234"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "ebbeytt+b234@amazon.com"
+    SSOUserFirstName          = "Ebbey"
+    SSOUserLastName           = "Thomas"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "ebbeytt+b234"
+    change_reason       = "New account creation"
+  }
+
+  account_customizations_name = "Infrastructure-ebbeytt+b234"
+}
