171a421e4e89178f69346ac22a469cb6ac960642
ebbsleo <35679628+ebbsleo@users.noreply.github.com>
2024-05-23 06:17:37 -0700

    Append new Terraform module for ebbeytta5

--- terraform/main.tf
+++ terraform/main.tf
@@ -74,3 +74,28 @@ module "ebbeytta321" {
 
   account_customizations_name = "Security-ebbeytta321"
 }
+
+
+module "ebbeytta5" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "ebbeytt+a5@amazon.com"
+    AccountName               = "ebbeytta5"
+    ManagedOrganizationalUnit = "Security"
+    SSOUserEmail              = "ebbeytt+a5@amazon.com"
+    SSOUserFirstName          = "Ebbey"
+    SSOUserLastName           = "Thomas"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Ebbey"
+    change_reason       = "AFT-update"
+  }
+
+  account_customizations_name = "Security Tooling-ebbeytta5"
+}
